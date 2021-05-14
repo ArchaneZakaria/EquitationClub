23d8f7976398c3ab194b8d7cb43611cade4d3dfb
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-05-14 15:31:17 +0000

    Ajout tableau fonctionnalités générale mais probleme Data

--- Client/src/views/app/clients/Clients.js
+++ Client/src/views/app/clients/Clients.js
@@ -25,6 +25,8 @@ import * as yup from "yup";
 import CIcon from "@coreui/icons-react";
 import { DocsLink } from "src/reusable";
 import ClientData from "../clients/ClientData";
+import infoClient from "../clients/InfoClient";
+
 
 import ReactDOM from "react-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -64,6 +66,12 @@ const fields = [
   "typeforfait",
   "StatusP",
   "options",
+];
+const fieldsInfo = [
+  "NomComplet",
+  "CIN",
+  "NombreHeures",
+  "NombreChevaux",
 ];
 
 const ModalDelete = (props) => {
@@ -115,6 +123,7 @@ const ModalDelete = (props) => {
 
 const ModalInfo = (props) => {
   const [modal, setModal] = React.useState(props.showing);
+  const [data, setData] = useState(infoClient);
   return (
     <React.Fragment>
       <CButton
@@ -133,13 +142,29 @@ const ModalInfo = (props) => {
           <CModalTitle >Informations générales</CModalTitle>
         </CModalHeader>
         <CModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+        <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              Information Générales :
+            </CCardHeader>
+            <CCardBody>
+            <CDataTable 
+                tableFilter
+                clickableRows
+              items={data}
+              fields={fieldsInfo}
+              hover
+              striped
+              bordered
+              size="sm"
+              itemsPerPage={10}
+              pagination
+            />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
         </CModalBody>
         <CModalFooter>
           <CButton color="secondary" onClick={() => setModal(false)}>
