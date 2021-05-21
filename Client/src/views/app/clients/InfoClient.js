c0ab228a4aa9ae807c6a4cfdc30cdeefbe795913
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-05-21 16:27:26 +0100

    Info client ajout et facturations

---
+++ Client/src/views/app/clients/InfoClient.js
@@ -0,0 +1,6 @@
+const infoClient = [
+  {id: 0, datePremiereInscription:"02/10/2015",nombreSeances:"1",nombrePensions:"2"},
+  {id: 1, datePremiereInscription:"16/10/2023",nombreSeances:"2",nombrePensions:"2"},
+  ]
+  
+  export default infoClient
