066a7bc9d134550f9f150695c0b7a593572ad26d
Medos <73502818+Medos-dev@users.noreply.github.com>
2021-05-29 13:59:38 +0100

    Ajout history cheval

---
+++ Client/src/views/app/chevaux/historyCheval.js
@@ -0,0 +1,8 @@
+const historyData = [
+    {id: 0,Cheval:"Epona", Client: 'Lahiyane Mehdi', Type: 'Séance',Description:'testet',DureeEnH:3, Date: '2021/03/01',Remarque:'Infoy'},
+    {id: 1, Cheval:"ARABICA1",Client: 'Archane Zakaria', Type: 'Entrainement',Description:'testet',DureeEnH:1, Date: '2021/03/01',Remarque:'Infoyr'},
+    {id: 2,Cheval:"ZETA", Client: 'Touil Hamza', Type: 'Séance',Description:'testet',DureeEnH:2, Date: '2021/03/01',Remarque:'Inf-o'},
+    {id: 3, Cheval:"MEDOS",Client: 'Radouine Ismail', Type: 'Elevage',Description:'testet',DureeEnH: 4 , Date: '2021/03/01',Remarque:'Infrto'},
+  ]
+
+export default historyData;
